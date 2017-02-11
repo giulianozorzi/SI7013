70a8ddbba86405824757f0a77cf9ada4b8ba80d2
Giuliano Zorzi <giulianozorzi@Mac-mini-di-Giuliano.local>
2017-02-11 15:14:44 +0100

    first version

---
+++ index.js
@@ -0,0 +1,106 @@
+var noble = require('noble');
+var moment = require('moment');
+
+var sensors=null;
+try {
+  sensors=require('./sensors.json');
+} catch(err) {
+  console.log('Scanning... Use the UUIDs below to configure the sensors.json file');
+  setTimeout(function(){
+    terminate();
+  },60*1000);
+}
+
+// names for the logs
+var characteristics_names = {
+  "2a19":"Battery level",
+  "ff92":"Temperature/Humidity"
+};
+
+// services and characteristics filters
+var serviceUUIDs = [];
+var characteristicUUIDs = ["2a19","ff92"];
+
+// set to number of seconds to limit execution time
+var maxrun=-1;
+
+// intervat to check battery status
+var batteryInterval=60*1000;
+
+if (maxrun>0) {
+  setTimeout(function(){
+    terminate();
+  },maxrun);
+}
+
+noble.on('stateChange', function(state) {
+  if (state === 'poweredOn') {
+    noble.startScanning();
+  } else {
+    noble.stopScanning();
+  }
+});
+
+noble.on('discover', function(peripheral) {
+  if (!sensors || sensors.length==0 || sensors.indexOf(peripheral.id)>-1) {
+    console.log(moment().toISOString(),'Found ',peripheral.advertisement.localName,'(' , peripheral.id , ')');
+    getServices(peripheral);
+  }
+});
+
+function getServices(peripheral) {
+  peripheral.connect(function(err) {
+    if (err) {
+      console.log(moment().toISOString(),'Error ',err,'connectinh to ',peripheral.advertisement.localName);
+    } else {
+      peripheral.discoverSomeServicesAndCharacteristics(serviceUUIDs,characteristicUUIDs,function(error, services,characteristics){
+        if (err) {
+          console.log(moment().toISOString(),'Discovery error',err,'for',peripheral.advertisement.localName);
+        } else {
+          characteristics.forEach(function(characteristic){
+            if (characteristic.uuid=="ff92") {
+              characteristic.notify(true, function(error) {
+                if (error) {
+                  console.log(moment().toISOString(),'Notification error');
+                } else {
+                  //console.log(peripheral.advertisement.localName,'notification on',characteristic.uuid);
+                  characteristic.on('read', function(data, isNotification) {
+                    var t=[data.readUIntBE(0, 1),data.readUIntBE(2, 1)].join('.');
+                    var h=data.readUIntBE(1, 1);
+                    console.log(moment().toISOString(),peripheral.advertisement.localName,characteristics_names[characteristic.uuid],t,h);
+                  });
+                }
+              });
+            } else if (characteristic.uuid=="2a19") {
+              characteristic.read(function(err,data) {
+                if (err) {
+                  console.log(moment().toISOString(),'Error reading',characteristic,peripheral.advertisement.localName);
+                } else {
+                  var b=data.readUIntBE(0, 1);
+                  console.log(moment().toISOString(),peripheral.advertisement.localName,characteristics_names[characteristic.uuid],b);
+                }
+              });
+              setInterval(function(){
+                characteristic.read(function(err,data) {
+                  if (err) {
+                    console.log(moment().toISOString(),'Error reading',characteristic,peripheral.advertisement.localName);
+                  } else {
+                    var b=data.readUIntBE(0, 1);
+                    console.log(moment().toISOString(),peripheral.advertisement.localName,characteristics_names[characteristic.uuid],b);
+                  }
+                });
+              },batteryInterval);
+            }             
+          });
+
+        }
+      }); 
+    }
+  });
+}
+
+function terminate() {
+  noble.stopScanning();
+  process.exit();
+}
+
